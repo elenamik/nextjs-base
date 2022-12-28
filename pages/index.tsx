7625767b7d63dba0ddbd34047461f23bbe4e12e1
Lena Mikhaylova <44687983+elenamik@users.noreply.github.com>
2022-12-28 12:29:06 -0500

    fixed sizing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,41 @@
 import CardGrid from "components/CardGrid";
-import { cardData3, cardData7, cardData9 } from "constants/mock/cards";
+import { useEffect, useState } from "react";
+import { CardData } from "types/card";
 
 export default function Home() {
-  // const [input, setInput] = useState(3);
-  // const [cards, setCards] = useState(cardData3);
-  //
-  // useEffect(() => {
-  //   let newCards = [];
-  //   for (let i = 0; i < input; i++) {
-  //     newCards.push({
-  //       subtitle: `SUBTITLE ${i + 1}`,
-  //       content: `CONTENT ${i + 1}`,
-  //     });
-  //   }
-  //   setCards(newCards);
-  // }, [input]);
+  const [input, setInput] = useState(3);
+  const [cards, setCards] = useState<CardData[]>([]);
+
+  useEffect(() => {
+    let newCards: CardData[] = [];
+    for (let i = 0; i < input; i++) {
+      newCards.push({
+        subtitle: `SUBTITLE ${i + 1}`,
+        content: `CONTENT ${i + 1}`,
+      });
+    }
+    setCards(newCards);
+  }, [input]);
 
   return (
-    <div className="flex flex-col justify-items-center pb-40">
-      {/*<input*/}
-      {/*  className="w-32"*/}
-      {/*  type="number"*/}
-      {/*  onChange={(e) => {*/}
-      {/*    // @ts-ignore*/}
-      {/*    setInput(e.target.value);*/}
-      {/*  }}*/}
-      {/*  value={input}*/}
-      {/*/>*/}
-      <h1 className="px-10 pt-10 text-left">3 Cards</h1>
-      <CardGrid data={cardData3} />
-
-      <h1 className="px-10 pt-10 text-left">7 Cards</h1>
-      <CardGrid data={cardData7} />
-
-      <h1 className="px-10 pt-10 text-left">9 Cards</h1>
-      <CardGrid data={cardData9} />
-
-      <h1 className="px-10 pt-10 text-left">9 Cards</h1>
-      <CardGrid data={cardData9} />
+    <div className="grid p-4 pt-10 pb-40">
+      <div className="flex justify-start">
+        <h1 className="p-1 px-10 text-left">Select Cards:</h1>
+        <input
+          className="w-32 p-1 text-left"
+          type="number"
+          onChange={(e) => {
+            const val = e.target.value as unknown as number;
+            if (val > 0) {
+              setInput(val);
+            }
+          }}
+          value={input}
+        />
+      </div>
+      <div className="flex justify-center">
+        <CardGrid data={cards} />
+      </div>
     </div>
   );
 }
